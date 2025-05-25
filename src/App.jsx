0541b58264590fdaa93c4f4f5c6753f3ec6065a7
ChibaweM <chibawemkwenya@gmail.com>
2025-05-25 03:27:18 +0200

    got the list of tracks

--- src/App.jsx
+++ src/App.jsx
@@ -3,45 +3,65 @@ import spotifyLogo from "./assets/spotify.svg";
 import youtubeLogo from "./assets/youtube.svg";
 import arrow from "./assets/arrow.svg";
 import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [playlistUrl, setplaylistUrl] = useState("");
+  const [tracks, setTracks] = useState([]);
+
+  const handleConvert = (e) => {
+    e.preventDefault();
+    const playlistId = playlistUrl.split("/").pop();
+
+    axios
+      .get("http://localhost:3000/playlist/" + playlistId)
+      .then((resp) => {
+        setTracks(resp.data);
+      })
+      .catch((err) => {
+        console.error("failed to:", err);
+      });
+
+    console.log(tracks);
+  };
 
   return (
-    <>
+    <section>
       <div>
         <h1>Spotify to Youtube Playlist Convertor</h1>
         <a href="https://spotify.com" target="_blank">
-          <img src={spotifyLogo} className="logo" alt="spotify logo" />
+          <img src={spotifyLogo} className="logo spotify" alt="spotify logo" />
         </a>
-        <a href="#">
-          <img src={arrow} className="logo arrow" alt="arrow" />
-        </a>
+
+        <img src={arrow} className="logo" alt="arrow" />
+
         <a href="https://youtube.com" target="_blank">
           <img src={youtubeLogo} className="logo youtube" alt="youtube logo" />
         </a>
       </div>
       <div>
-        <form>
+        <form onSubmit={handleConvert}>
           <p>Please paste a link to your playlist</p>
+          <p>!Only public playlists please!</p>
           <input
             type="text"
+            id="playlistUrl"
             placeholder="Enter your Spotify playlist URL"
             autoComplete="off"
             required
-            name="playlist-url"
+            value={playlistUrl}
+            onChange={(e) => setplaylistUrl(e.target.value)}
+            name="playlistUrl"
           />
           <div className="card">
-            <button onClick={() => setCount((count) => count + 1)}>
-              Convert
-            </button>
+            <button type="submit">Convert</button>
           </div>
         </form>
+        <ul>
+          {tracks}
+        </ul>
       </div>
-      <div className="card">
-        <h2>{count}</h2>
-      </div>
-    </>
+    </section>
   );
 }
 
